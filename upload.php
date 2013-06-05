4e0f9c9056021f62de1f9bfd0de9066dd86baa24
Jonathan Potter <potterjs61490@gmail.com>
2013-06-05 02:34:11 -0700

    Added feedback in upload.php.

--- upload.php
+++ upload.php
@@ -5,7 +5,7 @@
 	if ($file["error"] > 0)
 		echo "Error: " . $file["error"] . ".";
 	else if ($file["type"] != "audio/mid")
-		echo "Error: file must be MIDI.";
+		echo "Error: file must be MIDI (type is " . $file["type"] . ").";
 	else if (in_array ($file["name"], get_midi_filenames()))
 		echo "Error: a MIDI file with that name has already been uploaded.";
 	else if (!copy($file["tmp_name"], "midi/" . $file["name"]))
